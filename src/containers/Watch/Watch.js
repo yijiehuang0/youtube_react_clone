3f714cc5d1c94e7cf80a49fc436cd71ece166902
EJ Huang <yijie.huang@berkeley.edu>
2020-06-14 13:36:21 -0400

    issue: something wrong with the sizing of the video component

--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -2,9 +2,35 @@ import React from "react";
 import "./Watch.scss";
 import { RelatedVideos } from "../../components/RelatedVideos/RelatedVideos";
 import { VideoPreview } from "../../components/VideoPreview/VideoPreview";
+import { Video } from "../../components/Video/Video";
 export class Watch extends React.Component {
   render() {
-    return <RelatedVideos />;
+    return (
+      <div className="watch-grid">
+        <div className="watch-grid">
+          <Video className="video" id="-7fuHEEmEjs" />
+          <div
+            className="metadata"
+            style={{ width: "100%", height: "100px", background: "#F91112" }}
+          >
+            Metadata
+          </div>
+          <div
+            className="video-info-box"
+            style={{ width: "100%", height: "100px", background: "#BD10E0" }}
+          >
+            Video Info box
+          </div>
+          <div
+            className="comments"
+            style={{ width: "100%", height: "100px", background: "#9013FE" }}
+          >
+            comments
+          </div>
+          <RelatedVideos className="relatedVideos" />
+        </div>
+      </div>
+    );
   }
 }
 export default Watch;
